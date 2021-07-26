3e0a4b9fe75455046fefac485a048d08e58bd5fe
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-07-26 12:34:01 +0300

    Created selector of getting OrderHistory with replaced pair names && changed storage of orderHistory

---
+++ src/redux/selectors/ws/get_order_history.js
@@ -0,0 +1,23 @@
+import _get from 'lodash/get'
+import _map from 'lodash/map'
+import { createSelector } from 'reselect'
+import { REDUCER_PATHS } from '../../config'
+import { getMarketsObject } from '../meta'
+
+const path = REDUCER_PATHS.WS
+
+const orderHistory = (state) => {
+  return _get(state, `${path}.orderHistory`, [])
+}
+
+const allMarkets = state => getMarketsObject(state)
+
+const orderHistoryWithReplacedPairs = createSelector([allMarkets, orderHistory], (markets, orders) => {
+  return _map(orders, (order) => {
+    const { symbol } = order
+    const currentMarket = markets[symbol]
+    return { ...order, symbol: currentMarket.uiID }
+  }, [])
+})
+
+export default orderHistoryWithReplacedPairs
