3d893e7e152233faede9d0c5ab1c722a7e6ba570
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2021-07-29 17:34:00 +0530

    cleanup

--- src/redux/actions/ui.js
+++ src/redux/actions/ui.js
@@ -46,14 +46,6 @@ export const setActiveMarket = market => ({
   },
 })
 
-export const setFilteredValueWithKey = (key, value) => ({
-  type: types.SET_FILTRED_VALUE,
-  payload: {
-    key,
-    value,
-  },
-})
-
 export const saveComponentState = ({
   layoutID,
   componentID,
