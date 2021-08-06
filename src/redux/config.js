a6872076f82fa899294272b36125de95f1517bd8
Jason Hasperhoven <martijn.hasperhoven@gmail.com>
2021-08-06 18:10:52 +0400

    Revert isElectronApp statement

--- src/redux/config.js
+++ src/redux/config.js
@@ -19,7 +19,7 @@ const MAX_STORED_TRADES = 25
 
 const PUB_WS_API_URL = process.env.REACT_APP_WS_API_URL || 'wss://api-pub.bitfinex.com/ws/2'
 
-const isElectronApp = process.env.REACT_APP_IS_ELECTRON_APP === 'trsue'
+const isElectronApp = process.env.REACT_APP_IS_ELECTRON_APP === 'true'
 
 const electronAppVersion = process.env.REACT_APP_BFX_HF_UI_VERSION
 
