021d4770ad3f95019b77ae6176697c707a578da0
Jason Hasperhoven <martijn.hasperhoven@gmail.com>
2021-08-10 16:42:24 +0400

    Handle web auth error

--- src/redux/sagas/notifications/is_api_error_notification.js
+++ src/redux/sagas/notifications/is_api_error_notification.js
@@ -15,6 +15,20 @@ export default function* isAPIErrorNotification(action = {}) {
     yield put(WSActions.authWrongAPIKeys(true))
     yield put(WSActions.authAPIValidating(false))
   }
+  if (status === 'error' && _includes(text, 'Authentication failed')) {
+    const { href, origin } = window.location
+    if (_includes(origin, 'localhost')) {
+      // eslint-disable-next-line no-alert
+      alert(`
+        1. Connect and log in on bitfinex staging
+        2. Visit: https://api.staging.bitfinex.com/honey
+        3. In the returned URL you will find the authToken query param.
+        4. Visit localhost with query param ?authToken={authToken}
+      `)
+    } else {
+      window.location.assign(`${origin}/login?back=${encodeURIComponent(href)}`)
+    }
+  }
   if (status === 'success' && _includes(text, 'Authenticated with Bitfinex')) {
     yield put(WSActions.authWrongAPIKeys(false))
     yield put(WSActions.authAPIValidating(false))
