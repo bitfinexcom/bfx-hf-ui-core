5a7e361f29847e725c2b093a698e4536b5815e59
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2021-08-12 17:26:41 +0530

    atomic/algo orders: truncate columns, update column width

--- src/components/AlgoOrdersTable/AlgoOrdersTable.columns.js
+++ src/components/AlgoOrdersTable/AlgoOrdersTable.columns.js
@@ -1,35 +1,37 @@
 import React from 'react'
 
+import { reactVirtualizedCellRenderer } from '../../util/ui'
+
 export default (authToken, cancelOrder, gaCancelOrder) => [{
   label: 'Name',
   dataKey: 'name',
   width: 90,
   flexGrow: 0.7,
-  cellRenderer: ({ rowData = {} }) => rowData.name,
+  cellRenderer: ({ rowData = {} }) => reactVirtualizedCellRenderer(rowData.name),
 }, {
   label: 'Context',
   dataKey: 'args._margin',
   width: 65,
   flexGrow: 0.65,
-  cellRenderer: ({ rowData = {} }) => (rowData.args?._margin ? 'Margin' : 'Exchange'),
+  cellRenderer: ({ rowData = {} }) => reactVirtualizedCellRenderer(rowData.args?._margin ? 'Margin' : 'Exchange'),
 }, {
   label: 'Created',
   dataKey: 'gid',
   width: 155,
   flexGrow: 1.55,
-  cellRenderer: ({ rowData = {} }) => new Date(+rowData.gid).toLocaleString(),
+  cellRenderer: ({ rowData = {} }) => reactVirtualizedCellRenderer(new Date(+rowData.gid).toLocaleString()),
 }, {
   label: 'Symbol',
   dataKey: 'args.symbol',
   width: 140,
   flexGrow: 1.4,
-  cellRenderer: ({ rowData = {} }) => rowData.args?.uiID,
+  cellRenderer: ({ rowData = {} }) => reactVirtualizedCellRenderer(rowData.args?.uiID),
 }, {
   label: 'Label',
   dataKey: 'label',
   width: 545,
   flexGrow: 5.4,
-  cellRenderer: ({ rowData = {} }) => rowData.label,
+  cellRenderer: ({ rowData = {} }) => reactVirtualizedCellRenderer(rowData.label),
 }, {
   dataKey: 'cid',
   width: 40,
