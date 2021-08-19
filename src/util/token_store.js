225efdbff3ed9887a4ae112b85f8298bb623a4f8
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2021-08-19 16:45:45 +0530

    remove auth token timestamp

--- src/util/token_store.js
+++ src/util/token_store.js
@@ -4,7 +4,7 @@ import {
   removeCookie,
 } from './cookies'
 
-import { BFX_TOKEN_COOKIE, BFX_TOKEN_COOKIE_TIMESTAMP } from '../constants/cookies'
+import { BFX_TOKEN_COOKIE } from '../constants/cookies'
 
 /**
  * tokenStore
@@ -13,37 +13,20 @@ import { BFX_TOKEN_COOKIE, BFX_TOKEN_COOKIE_TIMESTAMP } from '../constants/cooki
  *
  */
 
-// current authtoken TTL is 10 min, UI marks it at expired at 8 minutes so we can fetch sooner
-export const AUTH_TOKEN_TTL = 8 * 60 * 1000 // 8 min
-
 const tokenStore = () => {
   const token = getCookieValue(BFX_TOKEN_COOKIE)
-  let tokenTimestamp = Number(getCookieValue(BFX_TOKEN_COOKIE_TIMESTAMP))
-
-  // If token present, but not token timestamp, the token is set from backend
-  // We set the timestamp from frontend to avoid time gap between server and client
-  if (token && !tokenTimestamp) {
-    tokenTimestamp = Date.now()
-    setCookie(BFX_TOKEN_COOKIE_TIMESTAMP, tokenTimestamp)
-  }
 
   return {
     token,
-    tokenTimestamp,
 
     reset() {
       const newToken = getCookieValue(BFX_TOKEN_COOKIE)
-      const newTokenTimestamp = Number(getCookieValue(BFX_TOKEN_COOKIE_TIMESTAMP))
-
       this.token = newToken
-      this.tokenTimestamp = newTokenTimestamp
     },
 
     set(newToken) {
       this.token = newToken
-      this.tokenTimestamp = Date.now()
       setCookie(BFX_TOKEN_COOKIE, newToken)
-      setCookie(BFX_TOKEN_COOKIE_TIMESTAMP, this.tokenTimestamp)
     },
 
     get(raw = false) {
@@ -52,17 +35,9 @@ const tokenStore = () => {
         : this.token
     },
 
-    getTokenTimestamp(raw = false) {
-      return raw
-        ? Number(getCookieValue(BFX_TOKEN_COOKIE_TIMESTAMP))
-        : this.tokenTimestamp
-    },
-
     clear() {
       this.token = null
-      this.tokenTimestamp = null
       removeCookie(BFX_TOKEN_COOKIE)
-      removeCookie(BFX_TOKEN_COOKIE_TIMESTAMP)
     },
   }
 }
@@ -72,10 +47,5 @@ const tokenStoreInstance = tokenStore()
 export default tokenStoreInstance
 
 export const getAuthToken = (raw = false) => tokenStoreInstance.get(raw)
-export const getAuthTokenTimestamp = (raw = false) => tokenStoreInstance.getTokenTimestamp(raw)
-export const isAuthTokenExpired = (raw = false) => {
-  const timePassed = Date.now() - getAuthTokenTimestamp(raw)
-  return timePassed > AUTH_TOKEN_TTL
-}
 export const getAuthTokenPromise = () => Promise.resolve(tokenStoreInstance.get())
 export const clearAuthToken = () => tokenStoreInstance.clear()
