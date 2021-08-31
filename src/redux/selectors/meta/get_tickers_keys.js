cfc3995cae55063436e07ab4b4907610603b4b89
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2021-08-31 18:33:33 +0530

    tickerlist: hide securities pair, show only exchange/derivatives pairs

---
+++ src/redux/selectors/meta/get_tickers_keys.js
@@ -0,0 +1,18 @@
+import _keys from 'lodash/keys'
+import _filter from 'lodash/filter'
+import { createSelector } from 'reselect'
+import { reduxSelectors } from '@ufx-ui/bfx-containers'
+
+import getMarkets from './get_markets'
+
+const { getIsSecuritiesPair, getIsTradingPair, getIsDerivativePair } = reduxSelectors
+
+const getTickersKeys = createSelector(
+  getMarkets,
+  getIsTradingPair,
+  getIsDerivativePair,
+  getIsSecuritiesPair,
+  (markets, isTradingPair, isDerivativePair, isSecuritiesPair) => _filter(_keys(markets), (pair) => isTradingPair(pair) && isDerivativePair(pair) && !isSecuritiesPair(pair)),
+)
+
+export default getTickersKeys
