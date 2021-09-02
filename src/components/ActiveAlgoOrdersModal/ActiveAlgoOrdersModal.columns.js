fbe85236356787b38001af1d642d5596495132d3
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-09-02 15:41:44 +0300

    Trans: ActiveAlgoOrders

--- src/components/ActiveAlgoOrdersModal/ActiveAlgoOrdersModal.columns.js
+++ src/components/ActiveAlgoOrdersModal/ActiveAlgoOrdersModal.columns.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Checkbox } from '@ufx-ui/core'
 
-export default (onOrderSelect, isOrderSelected) => [{
+export default (onOrderSelect, isOrderSelected, t) => [{
   dataKey: 'algoID',
   width: 30,
   flexGrow: 0.3,
@@ -14,31 +14,31 @@ export default (onOrderSelect, isOrderSelected) => [{
   ),
   disableSort: true,
 }, {
-  label: 'Name',
+  label: t('table.name'),
   dataKey: 'name',
   width: 75,
   flexGrow: 0.75,
   cellRenderer: ({ rowData = {} }) => rowData.name,
 }, {
-  label: 'Context',
+  label: t('table.context'),
   dataKey: 'args._margin',
   width: 90,
   flexGrow: 0.9,
   cellRenderer: ({ rowData = {} }) => (rowData.args?._margin ? 'Margin' : 'Exchange'),
 }, {
-  label: 'Created',
+  label: t('table.created'),
   dataKey: 'gid',
   width: 125,
   flexGrow: 1.25,
   cellRenderer: ({ rowData = {} }) => new Date(+rowData.gid).toLocaleString(),
 }, {
-  label: 'Symbol',
+  label: t('table.symbol'),
   dataKey: 'args.symbol',
   width: 160,
   flexGrow: 1.6,
   cellRenderer: ({ rowData = {} }) => rowData.args?.symbol,
 }, {
-  label: 'Label',
+  label: t('table.label'),
   dataKey: 'label',
   width: 490,
   flexGrow: 4.9,
