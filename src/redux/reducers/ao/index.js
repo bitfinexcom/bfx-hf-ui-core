ecb94223d99645febdb2df914a0a9d3ca5d974a0
Andrii R <durexp@protonmail.ch>
2021-09-16 13:42:20 +0000

    Update src/redux/reducers/ao/index.js
    Co-authored-by: Bhoomi Joshi <bhoomijoshi31@gmail.com>

--- src/redux/reducers/ao/index.js
+++ src/redux/reducers/ao/index.js
@@ -75,7 +75,7 @@ function reducer(state = getInitialState(), action = {}) {
           ...state.aoParams,
           [symbol]: {
             ...state.aoParams?.[symbol],
-            [algoID]: _filter(state.aoParams?.[symbol]?.[algoID], (p) => p.id !== id),
+            [algoID]: _filter(state.aoParams?.[symbol]?.[algoID], (p) => p?.id !== id),
           },
         },
       }
