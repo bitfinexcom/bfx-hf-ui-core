6896ad1e35808441260ff10530045f6fddc70c04
Andrii R <durexp@protonmail.ch>
2021-09-16 13:42:35 +0000

    Update src/redux/selectors/ao/get_ao_params.js
    Co-authored-by: Bhoomi Joshi <bhoomijoshi31@gmail.com>

--- src/redux/selectors/ao/get_ao_params.js
+++ src/redux/selectors/ao/get_ao_params.js
@@ -3,6 +3,8 @@ import { REDUCER_PATHS } from '../../config'
 
 const path = REDUCER_PATHS.AOS
 
+const EMPTY_ARR = []
+
 export default (state) => {
-  return _get(state, `${path}.aoParams`, [])
+  return _get(state, `${path}.aoParams`, EMPTY_ARR)
 }
