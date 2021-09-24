06c822990d75f4b7d91d2c2ef0adaeecff3d652c
Blue Mouse <durexp@protonmail.ch>
2021-09-24 15:49:49 +0300

    fixed clear data & reset not wiping auto-login password

--- src/pages/Authentication/AuthenticationConnectingForm.js
+++ src/pages/Authentication/AuthenticationConnectingForm.js
@@ -4,8 +4,6 @@ const AuthenticationConnectingForm = () => (
   <div className='hfui-authenticationpage__content'>
     <h2>Honey Framework UI</h2>
     <p>Starting up app...</p>
-
-    <form className='hfui-authenticationpage__inner-form' />
   </div>
 )
 
