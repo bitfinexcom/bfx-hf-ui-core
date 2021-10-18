22340275ec07ef60adb1e965923a9c12a8e85226
Blue Mouse <durexp@protonmail.ch>
2021-10-18 18:55:40 +0300

    added ability to import / export strategies

--- src/components/StrategyEditor/components/StrategyEditorPanel.js
+++ src/components/StrategyEditor/components/StrategyEditorPanel.js
@@ -16,7 +16,7 @@ const MAX_STRATEGY_LABEL_LENGTH = 35
 
 const StrategyEditorPanel = ({
   dark, strategy, onRemove, moveable, children, strategyId, removeable, execRunning, strategyDirty,
-  onSaveStrategy, onOpenSelectModal, onOpenCreateModal, onOpenRemoveModal, strategies,
+  onSaveStrategy, onOpenSelectModal, onOpenCreateModal, onOpenRemoveModal, strategies, onExportStrategy, onImportStrategy,
 }) => {
   const { t } = useTranslation()
   const { id = strategyId, label: strategyName } = strategy || {}
@@ -70,26 +70,43 @@ const StrategyEditorPanel = ({
                 <p key='text'>{t('strategyEditor.newStrategy')}</p>,
               ]}
             />
-            {!_isEmpty(strategy) && (
-              <Button
-                onClick={onSaveStrategy}
-                disabled={!strategyDirty}
-                label={[
-                  <i key='icon' className='icon-save' />,
-                  <p key='text'>{t('ui.save')}</p>,
-                ]}
-              />
-            )}
-            {!_isEmpty(strategy) && (
+            {_isEmpty(strategy) ? (
               <Button
                 className='hfui-remove-strategy__btn'
-                onClick={onOpenRemoveModal}
-                disabled={!id}
+                onClick={onImportStrategy}
                 label={[
-                  <i key='icon' className='icon-delete1' />,
-                  <p key='text'>{t('ui.removeBtn')}</p>,
+                  <Icon key='icon' name='download' />,
+                  <p key='text'>{t('ui.import')}</p>,
                 ]}
               />
+            ) : (
+              <>
+                <Button
+                  onClick={onSaveStrategy}
+                  disabled={!strategyDirty}
+                  label={[
+                    <i key='icon' className='icon-save' />,
+                    <p key='text'>{t('ui.save')}</p>,
+                  ]}
+                />
+                <Button
+                  className='hfui-remove-strategy__btn'
+                  onClick={onOpenRemoveModal}
+                  disabled={!id}
+                  label={[
+                    <i key='icon' className='icon-delete1' />,
+                    <p key='text'>{t('ui.removeBtn')}</p>,
+                  ]}
+                />
+                <Button
+                  className='hfui-remove-strategy__btn'
+                  onClick={onExportStrategy}
+                  label={[
+                    <Icon key='icon' name='upload' />,
+                    <p key='text'>{t('ui.export')}</p>,
+                  ]}
+                />
+              </>
             )}
           </div>
         </div>
@@ -116,6 +133,8 @@ StrategyEditorPanel.propTypes = {
   onOpenSelectModal: PropTypes.func.isRequired,
   onOpenCreateModal: PropTypes.func.isRequired,
   onOpenRemoveModal: PropTypes.func.isRequired,
+  onExportStrategy: PropTypes.func.isRequired,
+  onImportStrategy: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
   strategies: PropTypes.objectOf(PropTypes.object).isRequired,
 }
