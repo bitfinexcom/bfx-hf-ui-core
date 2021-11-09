032c15578b41da86b9f3fc113630b3071ec37ee8
Blue Mouse <durexp@protonmail.ch>
2021-11-09 18:53:35 +0200

    improved light theme colours for paper trading switch

--- src/components/SwitchMode/SwitchMode.js
+++ src/components/SwitchMode/SwitchMode.js
@@ -2,15 +2,28 @@ import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import { ToggleSwitch } from 'react-dragswitch'
 
+import { THEMES } from '../../redux/selectors/ui'
+
+const SWITCH_THEME = {
+  [THEMES.LIGHT]: {
+    onColor: '#07aa8c',
+    offColor: '#818e9a',
+  },
+  [THEMES.DARK]: {
+    onColor: '#244e3f',
+    offColor: '#4c5a63',
+  },
+}
+
 const SwitchMode = ({
-  isPaperTrading, isTradingModeModalVisible, openTradingModeModal,
+  isPaperTrading, isTradingModeModalVisible, openTradingModeModal, settingsTheme,
 }) => (
   <ToggleSwitch
     checked={isPaperTrading}
     onChange={openTradingModeModal}
     disabled={isTradingModeModalVisible}
-    onColor='#244e3f'
-    offColor='#4c5a63'
+    onColor={SWITCH_THEME[settingsTheme].onColor}
+    offColor={SWITCH_THEME[settingsTheme].offColor}
     className='toggle-switch'
     aria-label='Switch Paper Trading mode'
   />
@@ -20,6 +33,7 @@ SwitchMode.propTypes = {
   openTradingModeModal: PropTypes.func.isRequired,
   isPaperTrading: PropTypes.bool.isRequired,
   isTradingModeModalVisible: PropTypes.bool.isRequired,
+  settingsTheme: PropTypes.oneOf([THEMES.LIGHT, THEMES.DARK]).isRequired,
 }
 
 export default memo(SwitchMode)
