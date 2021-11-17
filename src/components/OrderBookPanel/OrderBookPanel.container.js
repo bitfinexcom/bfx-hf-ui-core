ea66eca921260f09e4dbb135f4eca274b92c2951
Blue Mouse <durexp@protonmail.ch>
2021-11-17 19:44:40 +0200

    fixed a bug when isTradingTerminal var was incorrectly calculated due to custom layouts

--- src/components/OrderBookPanel/OrderBookPanel.container.js
+++ src/components/OrderBookPanel/OrderBookPanel.container.js
@@ -4,7 +4,6 @@ import { reduxSelectors } from '@ufx-ui/bfx-containers'
 import {
   getActiveMarket, getComponentState, getMarketComponents,
 } from '../../redux/selectors/ui'
-import { DEFAULT_TRADING_KEY } from '../GridLayout/GridLayout.helpers'
 import { getMarkets } from '../../redux/selectors/meta'
 import UIActions from '../../redux/actions/ui'
 
@@ -12,13 +11,11 @@ import OrderBookPanel from './OrderBookPanel'
 
 const mapStateToProps = (state = {}, ownProps = {}) => {
   const { layoutID, layoutI: id } = ownProps
-  const isTradingTerminal = layoutID === DEFAULT_TRADING_KEY
 
   return {
     activeMarket: getActiveMarket(state),
     savedState: getComponentState(state, layoutID, 'book', id),
     markets: getMarkets(state),
-    isTradingTerminal,
     allMarketBooks: getMarketComponents(state, 'book'),
     getCurrencySymbol: reduxSelectors.getCurrencySymbolMemo(state),
   }
