1e52fc133ef509aef92056a32b9e222ec136a02c
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2021-11-22 13:59:22 +0530

    orderform: fix enter/escape issues
    - fix: in orderform, select limit order, click help will open help modal, press escape will switch to order-selection view,
      again select limit order, now it always shows help-modal and can't see limit order inputs view
    - fix: orderform accepts escape/enter events when it is intended for any open modal

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -32,6 +32,7 @@ import ConnectingModal from './Modals/ConnectingModal'
 import UnconfiguredModal from './Modals/UnconfiguredModal'
 import SubmitAPIKeysModal from './Modals/SubmitAPIKeysModal'
 import OrderFormMenu from './OrderFormMenu'
+import { getIsAnyModalOpen } from '../../util/document'
 import { getAOs, getAtomicOrders } from './OrderForm.orders.helpers'
 
 import './style.css'
@@ -123,6 +124,11 @@ class OrderForm extends React.Component {
   }
 
   handleKeydown(e) {
+    // handle keys only when no modal is open, and it is orderform-details view
+    if (getIsAnyModalOpen() || !this.getIsOrderFormInputsView()) {
+      return
+    }
+
     const { isAlgoOrder } = this.state
     const { key } = e
     if (key === 'Escape') {
@@ -310,6 +316,20 @@ class OrderForm extends React.Component {
         },
       }))
     }
+  }
+
+  getIsOrderFormInputsView() {
+    const { apiClientState, apiCredentials } = this.props
+    const { currentLayout, helpOpen } = this.state
+
+    const apiClientConnected = apiClientState === 2
+    const apiClientConfigured = apiCredentials?.configured && apiCredentials?.valid
+    const isConnectedWithValidAPI = apiClientConnected && apiClientConfigured
+    const showOrderform = isConnectedWithValidAPI || !isElectronApp
+
+    const isOrderFormInputsView = !helpOpen && currentLayout && showOrderform
+
+    return isOrderFormInputsView
   }
 
   setFieldData(data) {
@@ -528,7 +548,7 @@ class OrderForm extends React.Component {
               </div>
             )}
 
-            {!helpOpen && currentLayout && showOrderform && [
+            {this.getIsOrderFormInputsView() && [
               <div className='hfui-orderform__layout-label' key='layout-label'>
                 <i
                   className='icon-back-arrow'
