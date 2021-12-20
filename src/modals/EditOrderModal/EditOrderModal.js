013986379005285ed5576d4df8d657f1fe6b19b4
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2021-12-20 22:21:11 +0530

    fix props

--- src/modals/EditOrderModal/EditOrderModal.js
+++ src/modals/EditOrderModal/EditOrderModal.js
@@ -245,7 +245,7 @@ EditOrderModal.propTypes = {
   gaEditAO: PropTypes.func.isRequired,
   cancelAlgoOrder: PropTypes.func.isRequired,
   submitAlgoOrder: PropTypes.func.isRequired,
-  markets: PropTypes.arrayOf(PropTypes.object).isRequired,
+  markets: PropTypes.objectOf(PropTypes.object).isRequired,
 }
 
 export default memo(EditOrderModal)
