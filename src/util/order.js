e1509d4b5439ffa74380b0f0eedddde9a46c84fd
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2021-12-22 15:21:33 +0530

    improve order status
    - show order context for atomic orders
    - show hidden/tif/reduceonly/postonly icon with status

--- src/util/order.js
+++ src/util/order.js
@@ -1,3 +1,23 @@
+import _includes from 'lodash/includes'
+import _replace from 'lodash/replace'
+
+export const ORDER_CONTEXT = {
+  EXCHANGE: 'EXCHANGE',
+  MARGIN: 'MARGIN',
+  DERIVATIVE: 'DERIVATIVE',
+}
+
+export const orderContext = ({ type, symbol, oco }, isDerivativePair) => {
+  const prefix = (oco) ? 'OCO ' : ''
+  const isExchange = _includes(type, 'EXCHANGE')
+  const _type = prefix + _replace(type, 'EXCHANGE', '')
+  if (isDerivativePair(symbol)) return `${ORDER_CONTEXT.DERIVATIVE} ${_type}`
+
+  return isExchange
+    ? `${ORDER_CONTEXT.EXCHANGE} ${_type}`
+    : `${ORDER_CONTEXT.MARGIN} ${_type}`
+}
+
 export const getAOContext = (rowData) => {
-  return rowData?.args?._futures ? 'Derivative' : rowData?.args?._margin ? 'Margin' : 'Exchange'
+  return rowData?.args?._futures ? ORDER_CONTEXT.DERIVATIVE : rowData?.args?._margin ? ORDER_CONTEXT.MARGIN : ORDER_CONTEXT.EXCHANGE
 }
