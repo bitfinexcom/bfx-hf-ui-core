d5311526d144c8c41c7fb0a5005951ec7f3f5f42
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-12-29 21:18:24 +0200

    Joyride: resolve issue with scroll

--- src/components/Joyride/Joyride.js
+++ src/components/Joyride/Joyride.js
@@ -7,24 +7,28 @@ import { getLocaleOptions } from './Joyride.helpers'
 const HoneyJoyride = ({ styles: _styles, ...props }) => {
   const { t } = useTranslation()
   const locale = useMemo(() => getLocaleOptions(t), [t])
-  const styles = useMemo(() => ({
-    ..._styles,
-    options: {
-      arrowColor: '#243d50',
-      backgroundColor: '#243d50',
-      overlayColor: 'rgba(0, 0, 0, 0.4)',
-      primaryColor: '#05bc97',
-      textColor: '#f2f9ff',
-      zIndex: 10000,
-      ..._styles?.options,
-    },
-  }), [_styles])
+  const styles = useMemo(
+    () => ({
+      ..._styles,
+      options: {
+        arrowColor: '#243d50',
+        backgroundColor: '#243d50',
+        overlayColor: 'rgba(0, 0, 0, 0.4)',
+        primaryColor: '#05bc97',
+        textColor: '#f2f9ff',
+        zIndex: 10000,
+        ..._styles?.options,
+      },
+    }),
+    [_styles],
+  )
 
   return (
     <Joyride
       continuous
       showProgress
       showSkipButton
+      disableScrollParentFix
       styles={styles}
       locale={locale}
       {...props}
