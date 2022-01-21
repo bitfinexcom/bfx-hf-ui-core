9ed9caa612222357b3555c13511e2edf7f9ad0b7
Blue Mouse <durexp@protonmail.ch>
2022-01-21 13:34:58 +0200

    fixed compiling issue

--- src/constants/variables.js
+++ src/constants/variables.js
@@ -1,4 +1,4 @@
-import { isElectronApp } from "../redux/config"
+import { isElectronApp } from '../redux/config'
 
 export const MAX_STRATEGY_LABEL_LENGTH = 150
 export const DONT_SHOW_DMS_MODAL_KEY = 'HF_UI_DONT_SHOW_DMS_MODAL'
