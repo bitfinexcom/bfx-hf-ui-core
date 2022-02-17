7edce7d992328f0e601464dab937ca27423d287d
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2022-02-17 20:55:15 +0530

    lint fix

--- src/components/AtomicOrdersTable/AtomicOrdersTable.container.js
+++ src/components/AtomicOrdersTable/AtomicOrdersTable.container.js
@@ -1,19 +1,14 @@
 import { connect } from 'react-redux'
-import Debug from 'debug'
 
 import { reduxSelectors } from '@ufx-ui/bfx-containers'
 import { getAuthToken, getAtomicOrders, getFilteredAtomicOrders } from '../../redux/selectors/ws'
 import { getMarketPair } from '../../redux/selectors/meta'
-import WSActions from '../../redux/actions/ws'
-import GAActions from '../../redux/actions/google_analytics'
 import UIActions from '../../redux/actions/ui'
 
 import AtomicOrdersTable from './AtomicOrdersTable'
 import { cancelOrder } from './AtomicOrdersTable.helpers'
 
 const { getIsDerivativePair } = reduxSelectors
-
-const debug = Debug('hfui:c:atomic-orders-table')
 
 const mapStateToProps = (state = {}, { activeFilter }) => ({
   authToken: getAuthToken(state),
