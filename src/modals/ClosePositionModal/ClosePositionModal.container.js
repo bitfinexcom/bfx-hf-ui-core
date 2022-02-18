fea09e2f1106382fb6b1c1c4248fa963c2bf6535
Blue Mouse <durexp@protonmail.ch>
2022-02-18 14:19:16 +0200

    using closePosition helper

--- src/modals/ClosePositionModal/ClosePositionModal.container.js
+++ src/modals/ClosePositionModal/ClosePositionModal.container.js
@@ -1,16 +1,11 @@
 import { connect } from 'react-redux'
-import { prepareAmount } from 'bfx-api-node-util'
-import Debug from 'debug'
 
 import UIActions from '../../redux/actions/ui'
-import WSActions from '../../redux/actions/ws'
 import { getIsClosePositionModalVisible, getClosePositionModalData } from '../../redux/selectors/ui'
 import { getMarketPair } from '../../redux/selectors/meta'
 import { getAuthToken } from '../../redux/selectors/ws'
-import orders from '../../orders'
+import { closePosition } from '../../components/PositionsTable/PositionsTable.helpers'
 import ClosePositionModal from './ClosePositionModal'
-
-const debug = Debug('hfui:m:close-position')
 
 const mapStateToProps = (state = {}) => ({
   visible: getIsClosePositionModalVisible(state),
@@ -23,18 +18,7 @@ const mapDispatchToProps = dispatch => ({
   changeClosePositionModalState: (isVisible) => {
     dispatch(UIActions.changeClosePositionModalState(isVisible, {}))
   },
-  closePosition: (authToken, position = {}) => {
-    const { symbol, amount, basePrice } = position
-    const { generateOrder } = orders.Market()
-
-    const packet = generateOrder({
-      amount: prepareAmount(-1 * amount),
-      reduceonly: true,
-    }, symbol, 'm')
-
-    debug('closing position on %s %f @ %f', symbol, amount, basePrice)
-    dispatch(WSActions.submitOrder(authToken, packet))
-  },
+  closePosition: (authToken, position = {}) => closePosition(authToken, position, dispatch),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ClosePositionModal)
