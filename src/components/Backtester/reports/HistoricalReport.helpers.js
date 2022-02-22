3b9e5b241996c4719130652b65e2b9fc6e5842e9
Blue Mouse <durexp@protonmail.ch>
2022-02-22 15:01:42 +0200

    added support for exporting multiple csv tables

--- src/components/Backtester/reports/HistoricalReport.helpers.js
+++ src/components/Backtester/reports/HistoricalReport.helpers.js
@@ -1,43 +1,41 @@
-import { ExportToCsv } from 'export-to-csv'
-import { formatNumber } from '@ufx-ui/utils'
+import csvExport from 'csv-export'
+import { saveAs } from 'file-saver'
 import _map from 'lodash/map'
 import _isEmpty from 'lodash/isEmpty'
 import _split from 'lodash/split'
-import { AMOUNT_DECIMALS, PRICE_SIG_FIGS } from '../../../constants/precision'
+import _replace from 'lodash/replace'
 
-const TRADE_CSV_OPTIONS = {
-  fieldSeparator: ',',
-  quoteStrings: '"',
-  decimalSeparator: '.',
-  showLabels: true,
-  showTitle: true,
-  useTextFile: false,
-  useBom: true,
+const getExportFilename = (activeMarket) => {
+  // turn something like 2022-02-22T12:55:03.800Z into 2022-02-22T12-55-03
+  const date = _replace(_split(new Date().toISOString(), '.')[0], /:/g, '-')
+  return `${activeMarket}-${date}.zip`
 }
 
-const onTradeExportClick = (trades, activeMarket, t) => {
-  if (_isEmpty(trades)) {
+const onTradeExportClick = (rawTrades, activeMarket, t) => {
+  if (_isEmpty(rawTrades)) {
     return
   }
 
-  const csvExporter = new ExportToCsv({
-    ...TRADE_CSV_OPTIONS,
-    headers: [t('table.price'), t('table.amount'), t('table.pl'), t('table.label'), t('table.time')],
-    filename: `${activeMarket}-${_split(new Date().toISOString(), '.')[0]}`,
-    title: `Backtest ${activeMarket} trades report`,
-  })
-
-  const processedTrades = _map(trades, ({
+  const trades = _map(rawTrades, ({
     price, amount, pl, label, mts,
   }) => ({
-    price: formatNumber({ number: price, significantFigures: PRICE_SIG_FIGS, useGrouping: true }),
-    amount: formatNumber({ number: amount, decimals: AMOUNT_DECIMALS, useGrouping: true }),
-    pl: formatNumber({ number: pl, decimals: AMOUNT_DECIMALS, useGrouping: true }),
-    label,
-    mts: new Date(mts).toLocaleString(),
+    [t('table.price')]: price,
+    [t('table.amount')]: amount,
+    [t('table.pl')]: pl,
+    [t('table.label')]: label,
+    [t('table.time')]: _replace(new Date(mts).toLocaleString(), ',', ''),
   }))
 
-  csvExporter.generateCsv(processedTrades)
+  const documents = {
+    trades,
+  }
+
+  csvExport.export(documents, (buffer) => {
+    const blob = new Blob([buffer], { type: 'application/zip' })
+    const filename = getExportFilename(activeMarket)
+
+    saveAs(blob, filename)
+  })
 }
 
 export {
