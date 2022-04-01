620e6361685276810d8c040e8e1574c4486f4cff
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2022-04-01 14:51:56 +0530

    fix get

--- src/util/autologin.js
+++ src/util/autologin.js
@@ -36,7 +36,7 @@ export function getStoredPassword() {
     return localStorage.getItem(PASS)
   }
   console.log('getStoredPassword: else')
-  const res = window.electronService.getKeyFromEStore(PASS)
-  console.log('res: ', res)
-  return res
+  const result = window.electronService.getKeyFromEStore()[PASS] || {}
+  console.log('result: ', result)
+  return result?.[PASS]
 }
