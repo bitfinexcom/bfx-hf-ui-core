a9ce30ea01632f9ca7ffa6a85e43f0f40c226857
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2022-04-04 22:15:38 +0300

    ActiveStrategiesList

---
+++ src/components/StrategiesListTable/ActiveStrategiesList.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import _isEmpty from 'lodash/isEmpty'
+import { VirtualTable } from '@ufx-ui/core'
+import { useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import { getSortedByTimeStrategies } from '../../redux/selectors/ws'
+import StrategiesListColumns from './StrategiesList.columns'
+
+const ActiveStrategiesList = ({ onRowClick }) => {
+  const strategies = useSelector(getSortedByTimeStrategies)
+
+  const { t } = useTranslation()
+
+  return (
+    <>
+      {_isEmpty(strategies) ? (
+        <div className='no-trades__wrapper'>
+          <span className='no-trades__notification'>
+            {t('tradesTableModal.noActiveStrategies')}
+          </span>
+        </div>
+      ) : (
+        <VirtualTable
+          data={strategies}
+          columns={StrategiesListColumns(t)}
+          defaultSortBy='mts'
+          defaultSortDirection='DESC'
+          onRowClick={onRowClick}
+        />
+      )}
+    </>
+  )
+}
+
+ActiveStrategiesList.propTypes = {
+  onRowClick: PropTypes.func.isRequired,
+}
+
+export default ActiveStrategiesList
