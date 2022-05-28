848f6052e1e063ffcafb3c21f24d55e5ac6fd206
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2022-05-28 16:48:57 +0530

    fixing app shows bad connection modal immediately after login

--- src/redux/middleware/ws/on_close.js
+++ src/redux/middleware/ws/on_close.js
@@ -1,16 +1,18 @@
 import _isNil from 'lodash/isNil'
 import WSActions from '../../actions/ws'
 import UIActions from '../../actions/ui'
-import { getSocket } from '../../selectors/ws'
+import { getAuthToken, getSocket } from '../../selectors/ws'
 import { isElectronApp } from '../../config'
 
 export default (alias, store) => () => {
   const state = store.getState()
   const socket = getSocket(state, alias)
+  const isLoggedIn = !!getAuthToken(state)
 
   // do not show bad-connection-modal when connecting first time and it fails i.e. when lastActivity = null
   // do not show in hosted mode since it re-attepmpts new connection
-  if (!_isNil(socket?.lastActivity) && isElectronApp) {
+  // do not mark bad connection if user is still on login screen and ws onClose is triggered
+  if (!_isNil(socket?.lastActivity) && isElectronApp && isLoggedIn) {
     store.dispatch(UIActions.changeBadInternetConnectionState(true))
   }
 
