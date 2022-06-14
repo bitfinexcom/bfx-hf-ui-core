f436e92d9b8ff1f6e5d62b5f565ebb87fe9eca9c
Tarcisio Ferraz <tarcisio.ferraz@bitfinex.com>
2022-06-14 16:13:15 -0300

    refactor on update long

--- src/components/StrategyEditor/templates/vwap_example_eth_usd/onUpdateLong.js
+++ src/components/StrategyEditor/templates/vwap_example_eth_usd/onUpdateLong.js
@@ -5,50 +5,34 @@ export default `({ HFS }) => async (state = {}, update = {}) => {
   const iv = HFS.indicatorValues(state)
   const lastTrade = _.last(state.trades)
   const { vwap, s, l, roc } = iv
+  let label, tag
   
   if (lastTrade.tag === 'elvwap' && l - price > 0.7 && s > l) {
-    return HFS.closePositionMarket(state, {
-      price,
-      mtsCreate: mts,
-      label: 'close long | rev | l-p>1 & s<l',
-      tag: 'clvwap',
-    })
+    label = 'close long | rev | l-p>1 & s<l'
+    tag = 'clvwap'
   } else if (lastTrade.tag === 'elspike' && price > s) {
-    return HFS.closePositionMarket(state, {
-      price,
-      mtsCreate: mts,
-      label: 'close long spike',
-      tag: 'clspike',
-    })
+    label = 'close long spike'
+    tag = 'clspike'
   } else if (lastTrade.tag === 'elrocspike' && price < s && price > vwap) {
-    return HFS.closePositionMarket(state, {
-      price,
-      mtsCreate: mts,
-      label: 'close long | rev | p<s',
-      tag: 'clvwap',
-    })
+    label = 'close long | rev | p<s'
+    tag = 'clvwap'
   } else if (lastTrade.tag === 'elr' && price > vwap) {
-    return HFS.closePositionMarket(state, {
-      price,
-      mtsCreate: mts,
-      label: 'close long vwap',
-      tag: 'clvwap',
-    })
+    label = 'close long vwap'
+    tag = 'clvwap'
   } else if (price < vwap) {
-    return HFS.closePositionMarket(state, {
-      price,
-      mtsCreate: mts,
-      label: 'close long vwap',
-      tag: 'clvwap',
-    })
+    label = 'close long vwap'
+    tag = 'clvwap'
   } else if ((price < vwap - 0.25 && s < l - 0.4) || roc > 2.1) {
-    return HFS.closePositionMarket(state, {
-      price,
-      mtsCreate: mts,
-      label: 'close long | rev | roc',
-      tag: 'cl',
-    })
+    label = 'close long | rev | roc'
+    tag = 'cl'
+  } else {
+    return state
   }
   
-  return state
+  return HFS.closePositionMarket(state, {
+    price,
+    mtsCreate: mts,
+    label,
+    tag
+  })
 }`
