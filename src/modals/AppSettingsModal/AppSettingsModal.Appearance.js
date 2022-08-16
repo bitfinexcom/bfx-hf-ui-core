e97bd7c436df36b07023aa2d2ea8f7783f5783c9
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2022-08-16 12:39:08 +0300

    added checkbox min to tray on close

--- src/modals/AppSettingsModal/AppSettingsModal.Appearance.js
+++ src/modals/AppSettingsModal/AppSettingsModal.Appearance.js
@@ -6,24 +6,32 @@ import { useTranslation } from 'react-i18next'
 import _values from 'lodash/values'
 import _map from 'lodash/map'
 
+import { Checkbox } from '@ufx-ui/core'
 import WSActions from '../../redux/actions/ws'
 import GAActions from '../../redux/actions/google_analytics'
 import LanguageSettings from '../../components/Navbar/Navbar.LanguageSettings'
 import Dropdown from '../../ui/Dropdown'
 import {
-  SETTINGS_KEYS, getThemeSetting, THEMES,
+  SETTINGS_KEYS,
+  getThemeSetting,
+  THEMES,
 } from '../../redux/selectors/ui'
+import { isElectronApp } from '../../redux/config'
 
 const Appearance = () => {
   const { t } = useTranslation()
   const dispatch = useDispatch()
   const settingsTheme = useSelector(getThemeSetting)
   const [currentTheme, setCurrentTheme] = useState(settingsTheme)
+  const [isMinimiseToTrayChecked, setIsMinimiseToTrayChecked] = useState()
 
-  const themes = useMemo(() => _map(_values(THEMES), (value) => ({
-    label: t(`appSettings.${value}`),
-    value,
-  })), [t])
+  const themes = useMemo(
+    () => _map(_values(THEMES), (value) => ({
+      label: t(`appSettings.${value}`),
+      value,
+    })),
+    [t],
+  )
 
   useEffect(() => {
     setCurrentTheme(settingsTheme)
@@ -39,9 +47,7 @@ const Appearance = () => {
   return (
     <div className='appsettings-modal__appearance_setting'>
       <div className='appsettings-modal__setting'>
-        <p>
-          {t('appSettings.language')}
-        </p>
+        <p>{t('appSettings.language')}</p>
         <LanguageSettings />
       </div>
       <div className='appsettings-modal__setting'>
@@ -52,6 +58,23 @@ const Appearance = () => {
           options={themes}
         />
       </div>
+
+      {isElectronApp && (
+        <>
+          <br />
+          <div className='appsettings-modal__setting'>
+            <Checkbox
+              onChange={() => {}}
+              label={t('appSettings.minimizeToTrayCheckbox')}
+              checked
+              className='appsettings-modal__checkbox'
+            />
+            <div className='appsettings-modal__description'>
+              {t('appSettings.minimizeToTrayText')}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
