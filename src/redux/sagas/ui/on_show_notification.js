04562d5f23a17a8c97b76e8e27764c7052c6c947
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2022-08-16 14:54:44 +0300

    close native notification on window visible

--- src/redux/sagas/ui/on_show_notification.js
+++ src/redux/sagas/ui/on_show_notification.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import { select } from 'redux-saga/effects'
-import i18n from '../../../locales/i18n'
 import { UI_KEYS } from '../../constants/ui_keys'
 import { getUIState } from '../../selectors/ui'
 
@@ -10,12 +9,19 @@ const restoreApplication = () => ipcHelpers.sendRestoreAppMessage()
 
 export default function* onShowNotification(action = {}) {
   const isApplicationHidden = yield select((state) => getUIState(state, UI_KEYS.isApplicationHidden))
+
   if (!isApplicationHidden) {
     return
   }
 
   const { payload: { notification: { text } } } = action
 
-  const nativeNotification = new Notification('Honey Bitfinex', { body: text, icon: '/HF-icon.png' })
+  const nativeNotification = new Notification('Bitfinex Honey', { body: text, icon: '/HF-icon.png' })
   nativeNotification.addEventListener('click', restoreApplication)
+
+  document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'visible') {
+      nativeNotification.close()
+    }
+  })
 }
