2d9fb3c8034004405a1044e2c6c801100b1de732
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2022-08-16 15:14:38 +0530

    fix broken market-data page

--- src/pages/MarketData/MarketData.container.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux'
-
-import MarketData from './MarketData'
-
-const mapStateToProps = {}
-
-const mapDispatchToProps = {}
-
-export default connect(mapStateToProps, mapDispatchToProps)(MarketData)
