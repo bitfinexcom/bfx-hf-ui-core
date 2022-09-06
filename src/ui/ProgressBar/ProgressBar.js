161d815f5b35559acc077161f72b8f89dd93ef65
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2022-09-06 23:53:15 +0300

    Added progress bar, customised for dark and light modes

---
+++ src/ui/ProgressBar/ProgressBar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ReactProgressBar from '@ramonak/react-progress-bar'
+import { useSelector } from 'react-redux'
+import { getThemeSetting, THEMES } from '../../redux/selectors/ui'
+import scssVariables from '../../variables.scss'
+
+const ProgressBar = ({ progress, ...props }) => {
+  const theme = useSelector(getThemeSetting)
+  const isDark = theme === THEMES.DARK
+
+  const {
+    backgroundColor,
+    greyColor,
+    lighterBackgroundColor,
+    darkerBackgroundColorLight,
+  } = scssVariables
+
+  const bgColor = isDark ? backgroundColor : greyColor
+  const baseBgColor = isDark
+    ? lighterBackgroundColor
+    : darkerBackgroundColorLight
+
+  return (
+    <ReactProgressBar
+      animateOnRender
+      completed={progress}
+      bgColor={bgColor}
+      baseBgColor={baseBgColor}
+      labelAlignment={progress < 10 ? 'left' : 'right'}
+      {...props}
+    />
+  )
+}
+
+ProgressBar.propTypes = {
+  progress: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+}
+
+export default ProgressBar
