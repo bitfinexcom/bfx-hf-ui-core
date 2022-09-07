282bb86e40e3b557a72798a880ef46276d3540a4
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2022-09-07 09:00:10 +0300

    AppUpdate: improved UX

--- src/components/AppUpdate/AppUpdate.js
+++ src/components/AppUpdate/AppUpdate.js
@@ -1,88 +1,120 @@
 import React, { useState, useEffect } from 'react'
 import cx from 'clsx'
 import { useTranslation } from 'react-i18next'
-import { isElectronApp } from '../../redux/config'
+import ProgressBar from '../../ui/ProgressBar'
+
 import './style.css'
 
 const ipcHelpers = window.electronService
 
 const UPDATE_STATES = {
   UPDATE_AVAILABLE: 'UPDATE_AVAILABLE',
+  UPDATE_DOWNLOADING: 'UPDATE_DOWNLOADING',
   UPDATE_DOWNLOADED: 'UPDATE_DOWNLOADED',
+  UPDATE_INSTALLED: 'UPDATE_INSTALLED',
+  UPDATE_ERROR: 'UPDATE_ERROR',
 }
 
 const AppUpdate = () => {
   const { t } = useTranslation()
 
   const [hideNotification, setHideNotification] = useState(true)
-  const [hideRestart, setHideRestart] = useState(true)
-  const [updateState, setUpdateState] = useState(null)
+  const [newVersion, setNewVersion] = useState(null)
+  const [downloadProgress, setDownloadProgress] = useState(0)
+  const [updatingState, setUpdatingState] = useState(null)
 
-  const onUpdateAvailable = () => {
-    setUpdateState(UPDATE_STATES.UPDATE_AVAILABLE)
+  const closeNotification = () => {
+    setHideNotification(true)
+    ipcHelpers?.sendClearAppUpdateTimerEvent()
+  }
+
+  const onUpdateAvailable = (_, args) => {
+    setNewVersion(args?.version)
+    setUpdatingState(UPDATE_STATES.UPDATE_AVAILABLE)
     setHideNotification(false)
   }
 
-  // eslint-disable-next-line
-  const onUpdateDownloaded = (_event, releaseNotes, releaseName) => {
-    setUpdateState(UPDATE_STATES.UPDATE_DOWNLOADED)
-    setHideRestart(false)
-    setHideNotification(false)
+  const onUpdateDownloading = (_, args) => {
+    setDownloadProgress(args?.percent)
+  }
+
+  const onUpdateDownloaded = () => {
+    setUpdatingState(UPDATE_STATES.UPDATE_DOWNLOADED)
+    setTimeout(() => {
+      ipcHelpers?.sendRestartAppEvent()
+    }, 3000)
+  }
+
+  const onUpdateError = () => {
+    setUpdatingState(UPDATE_STATES.UPDATE_ERROR)
+    setTimeout(closeNotification, 3000)
   }
 
   useEffect(() => {
-    if (ipcHelpers && isElectronApp) {
+    if (ipcHelpers) {
       ipcHelpers.addAppUpdateAvailableEventListener(onUpdateAvailable)
+      ipcHelpers.addAppUpdateDownloadProgressListener(onUpdateDownloading)
       ipcHelpers.addAppUpdateDownloadedEventListener(onUpdateDownloaded)
+      ipcHelpers.addAppUpdateErrorListener(onUpdateError)
 
       return () => {
-        ipcHelpers.removeAppUpdateAvailableEventListener(onUpdateAvailable)
-        ipcHelpers.removeAppUpdateDownloadedEventListener(onUpdateDownloaded)
+        ipcHelpers.removeAllAppUpdateEventListeners()
       }
     }
 
     return () => {} // consistent-return
   }, [])
 
-  const closeNotification = () => {
-    setHideNotification(true)
-    if (ipcHelpers) {
-      ipcHelpers.sendClearAppUpdateTimerEvent()
-    }
-  }
-
-  const restartApp = () => {
-    if (ipcHelpers) {
-      ipcHelpers.sendRestartAppEvent()
-    }
+  const onDownloadAndRestartButtonClick = () => {
+    ipcHelpers?.sendDownloadUpdateEvent()
   }
 
   return (
     <div
-      className={cx('hfui-app-update__notification', { hidden: hideNotification })}
+      className={cx('hfui-app-update__notification', {
+        hidden: hideNotification,
+      })}
     >
-      <p className='message'>
-        {updateState === UPDATE_STATES.UPDATE_AVAILABLE && t('appUpdate.available')}
-        {updateState === UPDATE_STATES.UPDATE_DOWNLOADED && t('appUpdate.downloaded')}
-      </p>
-      <div className='btn-group'>
-        <button
-          className='close-button'
-          type='button'
-          onClick={closeNotification}
-        >
-          {t('ui.closeBtn')}
-        </button>
-        <button
-          className={cx('restart', {
-            hidden: hideRestart,
-          })}
-          type='button'
-          onClick={restartApp}
-        >
-          {t('ui.restartBtn')}
-        </button>
-      </div>
+      {updatingState === UPDATE_STATES.UPDATE_AVAILABLE && (
+        <div>
+          <p className='message'>
+            {t('appUpdate.available', { version: newVersion })}
+          </p>
+          <div className='btn-group'>
+            <button className='close-button' type='button' onClick={onDownloadAndRestartButtonClick}>
+              {t('appUpdate.downloadButton')}
+            </button>
+            <button
+              className='close-button'
+              type='button'
+              onClick={closeNotification}
+            >
+              {t('appUpdate.remindMeButton')}
+            </button>
+          </div>
+        </div>
+      )}
+      {updatingState === UPDATE_STATES.UPDATE_DOWNLOADING && (
+        <div>
+          <p className='message'>{t('appUpdate.downloading')}</p>
+          <ProgressBar progress={downloadProgress} maxCompleted={100} />
+        </div>
+      )}
+      {updatingState === UPDATE_STATES.UPDATE_DOWNLOADED && (
+        <div>
+          <p className='message'>{t('appUpdate.downloaded')}</p>
+        </div>
+      )}
+      {updatingState === UPDATE_STATES.UPDATE_INSTALLED && (
+        <div>
+          <p className='message'>{t('appUpdate.updateInstalled')}</p>
+        </div>
+      )}
+      {updatingState === UPDATE_STATES.UPDATE_ERROR && (
+        <div>
+          <p className='message'>{t('appUpdate.updateError')}</p>
+        </div>
+      )}
     </div>
   )
 }
