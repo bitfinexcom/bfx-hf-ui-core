3e2d257d5eb23a9d756e748929c9d6b2e83816fb
Blue Mouse <durexp@protonmail.ch>
2022-10-24 11:18:43 +0300

    using HF random visitor ID

--- src/redux/sagas/ui/pendo_identify.js
+++ src/redux/sagas/ui/pendo_identify.js
@@ -1,13 +1,31 @@
 import { put } from 'redux-saga/effects'
+import { v4 as uuidv4 } from 'uuid'
 import Debug from 'debug'
 import UIActions from '../../actions/ui'
+import { LOCAL_STORAGE_UID } from '../../../constants/variables'
+import { LOCAL_STORAGE_I18N_KEY } from '../../../locales/i18n'
+
+const getPendoVisitorId = () => {
+  const visitorId = localStorage.getItem(LOCAL_STORAGE_UID)
+
+  if (visitorId) {
+    return visitorId
+  }
+
+  const newVisitorId = uuidv4()
+  localStorage.setItem(LOCAL_STORAGE_UID, newVisitorId)
+
+  return newVisitorId
+}
 
 const PENDO_API_KEY = process.env.REACT_APP_PENDO_API_KEY
+const PENDO_VISITOR_ID = getPendoVisitorId()
 
 const debug = Debug('hfui:pendo')
 
 export default function* ({ payload = {} }) {
   const { auid, mode } = payload
+  const language = localStorage.getItem(LOCAL_STORAGE_I18N_KEY) || 'en-US'
 
   const { pendo } = window
 
@@ -23,8 +41,13 @@ export default function* ({ payload = {} }) {
 
   try {
     const isPendoInitialized = pendo.isReady()
+
     if (isPendoInitialized) {
       pendo.identify({
+        visitor: {
+          id: PENDO_VISITOR_ID,
+          language,
+        },
         account: {
           id: auid,
           mode,
@@ -34,12 +57,19 @@ export default function* ({ payload = {} }) {
       yield put(UIActions.setPendoState(true, auid))
       return
     }
+
     pendo.initialize({
+      apiKey: PENDO_API_KEY,
+      visitor: {
+        id: PENDO_VISITOR_ID,
+        language,
+      },
       account: {
         id: auid,
         mode,
       },
     })
+
     debug('pendo initialized', auid)
     yield put(UIActions.setPendoState(true, auid))
   } catch (e) {
