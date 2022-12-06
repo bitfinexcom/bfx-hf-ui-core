f74249298c207c502d972f7d79b37489c9b52aea
Blue Mouse <durexp@protonmail.ch>
2022-12-06 18:21:08 +0000

    restructured AO table columns & added a 'more info' button

--- src/components/AlgoOrdersTable/AlgoOrdersTable.columns.js
+++ src/components/AlgoOrdersTable/AlgoOrdersTable.columns.js
@@ -16,18 +16,11 @@ export default ({
 }) => {
   const columns = [
     {
-      label: t('table.name'),
+      label: t('table.alias'),
       dataKey: 'name',
       width: 90,
       flexGrow: 0.7,
       cellRenderer: ({ rowData = {} }) => defaultCellRenderer(rowData.name),
-    },
-    {
-      label: t('table.context'),
-      dataKey: 'args._margin',
-      width: 85,
-      flexGrow: 0.85,
-      cellRenderer: ({ rowData = {} }) => defaultCellRenderer(getAOContext(rowData)),
     },
     {
       label: t('table.created'),
@@ -39,6 +32,16 @@ export default ({
       ),
     },
     {
+      label: t('table.lastActive'),
+      dataKey: 'createdAt',
+      width: 175,
+      flexGrow: 1.75,
+      cellRenderer: ({ rowData = {} }) => defaultCellRenderer(
+        'N/A',
+        // new Date(rowData.createdAt || +rowData.gid).toLocaleString(),
+      ),
+    },
+    {
       label: t('table.symbol'),
       dataKey: 'args.symbol',
       width: 150,
@@ -46,11 +49,22 @@ export default ({
       cellRenderer: ({ rowData = {} }) => defaultCellRenderer(getMarketPair(rowData.args?.symbol)),
     },
     {
-      label: t('table.label'),
-      dataKey: 'label',
-      width: 545,
-      flexGrow: 5.4,
-      cellRenderer: ({ rowData = {} }) => defaultCellRenderer(rowData.label),
+      label: '',
+      dataKey: '',
+      width: 300,
+      flexGrow: 3.0,
+      cellRenderer: () => (
+        <div className='hfui-aolist__wrapper_more_info'>
+          <Icon
+            name='info-circle'
+            aria-label='More info'
+            onClick={() => {}}
+          />
+          <span className='more_info_action'>
+            {t('table.moreInfo')}
+          </span>
+        </div>
+      ),
     },
   ]
 
