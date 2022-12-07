b9f3163bc1d646b956a191c707c8cde9535873f8
Blue Mouse <durexp@protonmail.ch>
2022-12-07 22:09:11 +0000

    added atomic order history title & export to JSON button

--- src/components/AlgoOrdersTable/AlgoOrdersTable.columns.js
+++ src/components/AlgoOrdersTable/AlgoOrdersTable.columns.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Icon } from 'react-fa'
+import _map from 'lodash/map'
+import _size from 'lodash/size'
 import OutsideClickHandler from 'react-outside-click-handler'
 import Button from '../../ui/Button'
 
 import { defaultCellRenderer } from '../../util/ui'
 import { Item } from '../Navbar/Navbar.LayoutSettings'
 import { getAOContext } from '../../util/order'
+
+const getOrderDetails = (rowData = {}) => {
+  const { args = {}, id } = rowData
+
+  switch (id) {
+    case 'bfx-twap':
+      return [
+        { label: 'Interval', value: `${args.sliceInterval}s` },
+        { label: 'Slice', value: args.sliceAmount },
+        { label: 'Target', value: args.priceCondition },
+      ]
+    default:
+      return []
+  }
+}
 
 export default ({
   authToken,
@@ -63,75 +80,113 @@ export default ({
       dataKey: '',
       width: 250,
       flexGrow: 2.5,
-      cellRenderer: ({ rowData = {} }) => (
-        <div>
-          <div
-            className='hfui-aolist__wrapper_more_info'
-            onClick={() => setMoreInfoGID(rowData.gid)}
-          >
-            <Icon
-              name='info-circle'
-              aria-label='More info'
-              onClick={() => { }}
-            />
-            <span className='more_info_action'>
-              {t('table.moreInfo')}
-            </span>
-          </div>
+      cellRenderer: ({ rowData = {} }) => {
+        const orderDetails = getOrderDetails(rowData)
+        const detailsSize = _size(orderDetails)
 
-          {moreInfoGID === rowData?.gid && (
-            <OutsideClickHandler onOutsideClick={() => setMoreInfoGID(null)}>
-              <div className='hfui-navbar__layout-settings__menu hfui-aolist__wrapper_order_info'>
-                <div className='title-container'>
-                  <div className='hfui-navbar__layout-settings__title'>
-                    {rowData?.name}
-                    <span className='sub-title'>
-                      {rowData?.label}
-                    </span>
+        return (
+          <div>
+            <div
+              className='hfui-aolist__wrapper_more_info'
+              onClick={() => setMoreInfoGID(rowData.gid)}
+            >
+              <Icon
+                name='info-circle'
+                aria-label='More info'
+                onClick={() => { }}
+              />
+              <span className='more_info_action'>
+                {t('table.moreInfo')}
+              </span>
+            </div>
+
+            {console.log(rowData)}
+
+            {moreInfoGID === rowData?.gid && (
+              <OutsideClickHandler onOutsideClick={() => setMoreInfoGID(null)}>
+                <div className='hfui-navbar__layout-settings__menu hfui-aolist__wrapper_order_info'>
+                  <div className='title-container'>
+                    <div className='hfui-navbar__layout-settings__title'>
+                      {rowData?.name}
+                      <span className='sub-title'>
+                        {rowData?.label}
+                      </span>
+                    </div>
+                    <Button
+                      onClick={() => setMoreInfoGID(null)}
+                      key='close-btn'
+                      className='close-btn'
+                      label={[
+                        <p key='text'>&#10005;</p>,
+                      ]}
+                    />
                   </div>
-                  <Button
-                    onClick={() => setMoreInfoGID(null)}
-                    key='close-btn'
-                    className='close-btn'
-                    label={[
-                      <p key='text'>&#10005;</p>,
-                    ]}
-                  />
+                  <div className='basic-info'>
+                    <div className='info-col'>
+                      <span className='info-label'>
+                        {t('table.created')}
+                      </span>
+                      {' '}
+                      <span className='info-value'>
+                        {new Date(rowData.createdAt || +rowData.gid).toLocaleString()}
+                      </span>
+                    </div>
+                    <div className='info-col'>
+                      <span className='info-label'>
+                        {t('table.symbol')}
+                      </span>
+                      {' '}
+                      <span className='info-value'>
+                        {rowData?.args?.symbol}
+                      </span>
+                    </div>
+                    <div className='info-col'>
+                      <span className='info-label'>
+                        {t('table.context')}
+                      </span>
+                      {' '}
+                      <span className='info-value'>
+                        {getAOContext(rowData)}
+                      </span>
+                    </div>
+                  </div>
+                  <p className='info-title'>
+                    {t('table.orderDetails')}
+                  </p>
+                  <div className='basic-info'>
+                    {_map(orderDetails, ({ label, value }, index) => (
+                      <div className='info-col' key={label}>
+                        <span className='info-label'>
+                          {label}
+                        </span>
+                        =
+                        <span className='info-value'>
+                          {value}
+                        </span>
+                        {index !== detailsSize - 1 && ', '}
+                      </div>
+                    ))}
+                  </div>
+                  <div className='order-history'>
+                    <p className='order-history_title'>
+                      {t('table.atomicHistory')}
+                    </p>
+                    <div
+                      className='order-history_button'
+                      onClick={() => {}}
+                    >
+                      {t('table.exportJSON')}
+                    </div>
+                  </div>
+                  {/* <div className='order-history-table'>
+
+                  </div> */}
                 </div>
-                <div className='basic-info'>
-                  <div className='info-col'>
-                    <span className='info-label'>
-                      {t('table.created')}
-                    </span>
-                    {' '}
-                    <span className='info-value'>
-                      {new Date(rowData.createdAt || +rowData.gid).toLocaleString()}
-                    </span>
-                  </div>
-                  <div className='info-col'>
-                    <span className='info-label'>
-                      {t('table.symbol')}
-                    </span>
-                    {' '}
-                    <span className='info-value'>
-                      {rowData?.args?.symbol}
-                    </span>
-                  </div>
-                  <div className='info-col'>
-                    <span className='info-label'>
-                      {t('table.context')}
-                    </span>
-                    {' '}
-                    <span className='info-value'>
-                      {getAOContext(rowData)}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </OutsideClickHandler>
-          )}
-        </div>
-      ),
+              </OutsideClickHandler>
+            )}
+          </div>
+        )
+      },
     })
 
     // 'Actions' column
