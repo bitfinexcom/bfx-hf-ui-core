24a209ce05ece0e047d58ff37b9ffe92e97ae9c6
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2023-01-10 17:01:07 +0200

    RadioInput: use options object[] type

--- src/components/OrderForm/FieldComponents/input.radio.js
+++ src/components/OrderForm/FieldComponents/input.radio.js
@@ -6,28 +6,35 @@ import _map from 'lodash/map'
 import RadioButton from '../../../ui/RadioButton'
 import { renderString } from './fields.helpers'
 
-const RadioInput = memo(({
-  def: { options }, renderData, value, onChange, disabled,
-}) => (
-  <div className={ClassNames('hfui-orderform__input', { disabled })}>
-    {_map(options, (o, i) => (
-      <RadioButton
-        key={i} //eslint-disable-line
-        label={renderString(o, renderData)}
-        value={value === o}
-        onChange={() => onChange(o)}
-        uppercase
-      />
-    ))}
-  </div>
-))
+const RadioInput = memo(
+  ({
+    def: { options }, renderData, value, onChange, disabled,
+  }) => (
+    <div className={ClassNames('hfui-orderform__input', { disabled })}>
+      {_map(options, ({ value: v, label }, i) => (
+        <RadioButton
+          key={i}
+          label={renderString(label, renderData)}
+          value={value === v}
+          onChange={() => onChange(v)}
+          uppercase
+        />
+      ))}
+    </div>
+  ),
+)
 
 RadioInput.displayName = 'RadioInput'
 
 RadioInput.propTypes = {
-  def: PropTypes.objectOf(PropTypes.oneOfType([
-    PropTypes.string, PropTypes.bool, PropTypes.array,
-  ])).isRequired,
+  def: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.bool,
+      PropTypes.array,
+      PropTypes.object,
+    ]),
+  ).isRequired,
   renderData: PropTypes.shape({
     QUOTE: PropTypes.string.isRequired,
     BASE: PropTypes.string.isRequired,
