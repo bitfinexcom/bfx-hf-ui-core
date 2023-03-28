eed3de3a176c49982f27e8ae444db4981e17e595
Blue Mouse <durexp@protonmail.ch>
2023-03-28 14:05:04 +0100

    added advanced configuration tab

--- src/modals/HelpUsImproveHoney/HelpUsImproveHoney.container.js
+++ src/modals/HelpUsImproveHoney/HelpUsImproveHoney.container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
 
 import UIActions from '../../redux/actions/ui'
-import { getUIModalStateForKey } from '../../redux/selectors/ui'
+import { getUIModalStateForKey, getThemeSetting } from '../../redux/selectors/ui'
 // import { getAuthToken } from '../../redux/selectors/ws'
 import HelpUsImproveHoney from './HelpUsImproveHoney'
 import { UI_MODAL_KEYS } from '../../redux/constants/modals'
@@ -9,6 +9,7 @@ import { UI_MODAL_KEYS } from '../../redux/constants/modals'
 const mapStateToProps = (state = {}) => ({
   visible: getUIModalStateForKey(state, UI_MODAL_KEYS.HELP_US_IMPROVE_HONEY_MODAL),
   // authToken: getAuthToken(state),
+  settingsTheme: getThemeSetting(state),
 })
 
 const mapDispatchToProps = dispatch => ({
