f46f4d8ae0e66eecf8d276f208ca8d4e119404e9
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2023-04-25 00:29:05 +0300

    Opt-in in app settings

--- src/modals/AppSettingsModal/AppSettingsModal.Analytics.js
+++ src/modals/AppSettingsModal/AppSettingsModal.Analytics.js
@@ -1,13 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { memo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Checkbox } from '@ufx-ui/core'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 
 import WSActions from '../../redux/actions/ws'
 import GAActions from '../../redux/actions/google_analytics'
 import {
-  SETTINGS_KEYS, getOptinCrashReports, getOptinBFXAnalytics, getOptinVendorPendo,
+  SETTINGS_KEYS,
+  getOptinCrashReports,
+  getOptinBFXAnalytics,
+  getOptinVendorPendo,
 } from '../../redux/selectors/ui'
+import {
+  METRICS_CODE_REF_URL,
+  PENDO_CODE_REF_URL,
+  PENDO_PRIVACY_POLICY_URL,
+  PENDO_WEB_URL,
+  UNIQUE_ID_CODE_REF_URL,
+} from '../../redux/config'
 // import InnerModal from '../../ui/InnerModal/InnerModal'
 
 const Analytics = () => {
@@ -19,17 +30,23 @@ const Analytics = () => {
   const optinVendorPendo = useSelector(getOptinVendorPendo)
 
   const optinCrashReportsHandler = (isChecked) => {
-    dispatch(WSActions.saveSetting(SETTINGS_KEYS.OPT_IN_CRASH_REPORTS, isChecked))
+    dispatch(
+      WSActions.saveSetting(SETTINGS_KEYS.OPT_IN_CRASH_REPORTS, isChecked),
+    )
     dispatch(GAActions.updateSettings())
   }
 
   const optinBFXAnalyticsHandler = (isChecked) => {
-    dispatch(WSActions.saveSetting(SETTINGS_KEYS.OPT_IN_BFX_ANALYTICS, isChecked))
+    dispatch(
+      WSActions.saveSetting(SETTINGS_KEYS.OPT_IN_BFX_ANALYTICS, isChecked),
+    )
     dispatch(GAActions.updateSettings())
   }
 
   const optinVendorPendoHandler = (isChecked) => {
-    dispatch(WSActions.saveSetting(SETTINGS_KEYS.OPT_IN_VENDOR_PENDO, isChecked))
+    dispatch(
+      WSActions.saveSetting(SETTINGS_KEYS.OPT_IN_VENDOR_PENDO, isChecked),
+    )
     dispatch(GAActions.updateSettings())
   }
 
@@ -38,35 +55,85 @@ const Analytics = () => {
       <div className='appsettings-modal__setting appsettings-modal__setting--crash-reports'>
         <Checkbox
           onChange={optinCrashReportsHandler}
-          label={t('appSettings.optinCrashReports')}
+          label={t('helpUsImproveModal.unexpectedErrors')}
           checked={optinCrashReports}
           className='appsettings-modal__checkbox'
         />
         <div className='appsettings-modal__description'>
-          {t('appSettings.optinCrashReportsDesc')}
+          <Trans
+            t={t}
+            i18nKey='helpUsImproveModal.unexpectedErrorsDesc'
+            components={{
+              codeRefUrl: (
+                <a
+                  href={METRICS_CODE_REF_URL}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                />
+              ),
+            }}
+          />
         </div>
       </div>
       <div className='appsettings-modal__setting appsettings-modal__setting--crash-reports'>
         <Checkbox
           onChange={optinBFXAnalyticsHandler}
-          label={t('appSettings.optinCrashReports')}
+          label={t('helpUsImproveModal.anonymousData')}
           checked={optinBFXAnalytics}
           className='appsettings-modal__checkbox'
         />
         <div className='appsettings-modal__description'>
-          {t('appSettings.optinCrashReportsDesc')}
+          <Trans
+            t={t}
+            i18nKey='helpUsImproveModal.anonymousDataDesc'
+            components={{
+              codeRefUrl: (
+                <a
+                  href={UNIQUE_ID_CODE_REF_URL}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                />
+              ),
+            }}
+          />
         </div>
       </div>
       <div className='appsettings-modal__setting appsettings-modal__setting--crash-reports'>
         <Checkbox
           onChange={optinVendorPendoHandler}
-          label={t('appSettings.optinVendorPendo')}
+          label={t('helpUsImproveModal.pendoUsage')}
           checked={optinVendorPendo}
           className='appsettings-modal__checkbox'
         />
-        <div className='appsettings-modal__description'>
-          {t('appSettings.optinVendorPendoDesc')}
-        </div>
+        <p className='appsettings-modal__description'>
+          <Trans
+            t={t}
+            i18nKey='helpUsImproveModal.pendoUsageDesc'
+            components={{
+              pendoURL: (
+                <a
+                  href={PENDO_WEB_URL}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                />
+              ),
+              pendoPP: (
+                <a
+                  href={PENDO_PRIVACY_POLICY_URL}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                />
+              ),
+              codeRefUrl: (
+                <a
+                  href={PENDO_CODE_REF_URL}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                />
+              ),
+            }}
+          />
+        </p>
       </div>
     </div>
   )
