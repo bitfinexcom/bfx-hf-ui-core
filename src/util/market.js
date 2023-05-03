1b312233360865db000b789c843a54e6c01b49d6
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2023-05-04 01:26:44 +0300

    getIsPaperPair util fn

--- src/util/market.js
+++ src/util/market.js
@@ -5,10 +5,11 @@ import _includes from 'lodash/includes'
 import _replace from 'lodash/replace'
 import _isObject from 'lodash/isObject'
 import {
-  ALLOWED_PAPER_PAIRS,
   MAIN_MODE,
   PAPER_MODE,
 } from '../redux/reducers/ui'
+
+const PAPER_PAIR_PREFIX = 'TEST'
 
 export const getDefaultMarket = (markets) => _get(markets, [_first(_keys(markets))], 'uiID')
 
@@ -26,6 +27,6 @@ export const getCorrectIconNameOfPerpCcy = (perpCcy) => {
   return perpCcy
 }
 
-export const getStrategyModeForSymbol = (symbol) => (_includes(ALLOWED_PAPER_PAIRS, symbol?.wsID) ? PAPER_MODE : MAIN_MODE)
+export const getIsPaperPair = (symbol) => _includes(_isObject(symbol) ? symbol?.wsID : symbol, PAPER_PAIR_PREFIX)
 
-export const getIsPaperPair = (symbol) => _includes(ALLOWED_PAPER_PAIRS, _isObject(symbol) ? symbol?.wsID : symbol)
+export const getStrategyModeForSymbol = (symbol) => (getIsPaperPair(symbol) ? PAPER_MODE : MAIN_MODE)
