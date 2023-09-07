a97940ab12ac329bdbdfbeb50dbfdcb13c1025a3
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2023-09-07 14:39:54 +0300

    messageQueueWorker: avoid spamming actions if they are dispatched in a row

--- src/redux/sagas/ws/worker_message_queue.js
+++ src/redux/sagas/ws/worker_message_queue.js
@@ -1,4 +1,4 @@
-import { put, select } from 'redux-saga/effects'
+import { delay, put, select } from 'redux-saga/effects'
 import _keys from 'lodash/keys'
 import _some from 'lodash/some'
 import _includes from 'lodash/includes'
@@ -10,28 +10,40 @@ import UIActions from '../../actions/ui'
 import { UI_MODAL_KEYS } from '../../constants/modals'
 
 const debug = Debug('hfui:rx:s:ws-hfui:msg-q')
-const ONLINE_ONLY_MESSAGES = ['order.submit', 'algo_order.submit', 'api_credentials.save', 'strategy.execute_start', 'settings.update']
+const ONLINE_ONLY_MESSAGES = [
+  'order.submit',
+  'algo_order.submit',
+  'api_credentials.save',
+  'strategy.execute_start',
+  'settings.update',
+]
 let queue = []
+let isQueueExecuted = false
 
-// Place every outgoing message in a queue if connection is offline
-export default function* (action = {}) {
+export default function* messageQueueWorker(action = {}) {
   const sockets = yield select(getSockets)
-  const offline = _some(_keys(sockets), s => sockets[s].status !== SOCKET_STATUS_MAP.ONLINE)
+  const offline = _some(
+    _keys(sockets),
+    (s) => sockets[s].status !== SOCKET_STATUS_MAP.ONLINE,
+  )
 
   if (offline && _some(ONLINE_ONLY_MESSAGES, m => _includes(action.payload, m))) {
     yield put(UIActions.changeUIModalState(UI_MODAL_KEYS.NO_CONNECTION_MODAL, true))
     return
   }
-
   if (action.type !== WSTypes.FLUSH_QUEUE) {
     queue = [...queue, action]
+    if (isQueueExecuted) {
+      return
+    }
   }
 
   if (offline || queue.length === 0) {
     return
   }
-
-  debug('flushing %d messages', queue.length)
+  isQueueExecuted = true
+  yield delay(500)
+  debug('flushing %d messages')
 
   for (let i = 0; i < queue.length; i += 1) {
     const queuedAction = queue[i]
@@ -44,4 +56,5 @@ export default function* (action = {}) {
   }
 
   queue = []
+  isQueueExecuted = false
 }
